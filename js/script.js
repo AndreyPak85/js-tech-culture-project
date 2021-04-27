077c9494d37960fb8c886e79d436db96ae2bff7f
Andrey Pak <andrey.pak85@gmail.com>
2021-04-27 23:40:52 +0600

    complete popup 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const clubSelect = document.querySelector(".club-select");
 const clubSelectList = document.querySelector(".clubs-list ul");
 
 clubSelect.addEventListener("click", () => {
-    console.log(clubSelectList);
     if (clubSelectList.classList.contains("display-block")) {
         clubSelectList.classList.add("display-none"); 
         clubSelectList.classList.remove("display-block");
@@ -13,4 +12,31 @@ clubSelect.addEventListener("click", () => {
         clubSelectList.classList.remove("display-remove");
         console.log('removed')
     }
+})
+
+/** 
+ * @author AP 
+ * При клике на эту надпись открывается модальное окно id=’free_visit_form’
+ * При нажатии на крестик или подложку - исчезать.
+ */
+
+const freeVisit = document.querySelector(".free-visit p a");
+const freeVisitForm = document.querySelector("#free_visit_form");
+const formCloseImg = document.querySelectorAll(".close-form");
+const formOverlay = document.querySelectorAll(".overlay");
+
+freeVisit.addEventListener("click", () => {
+    freeVisitForm.classList.add("display-block");
+})
+
+formCloseImg.forEach(item => {
+    item.addEventListener("click", (e) => {
+        freeVisitForm.classList.remove("display-block");
+    })
+})
+
+formOverlay.forEach(item => {
+    item.addEventListener("click", () => {
+        freeVisitForm.classList.remove("display-block");
+    })
 })
